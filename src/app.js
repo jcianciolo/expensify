cfd35325d42d00698cdd5bd8c47823979a3f405d
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-03-31 19:27:47 -0700

    React-router 101

--- src/app.js
+++ src/app.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
 
-ReactDOM.render(<p>This is my boilerplate.</p>, document.getElementById('app'));
+const AddExpensePage = () => (
+    <div>
+        This is my add expense component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is my edit expense component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my help page component
+    </div>
+);
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
