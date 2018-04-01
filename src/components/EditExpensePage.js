1d78c7a999870e68c1e3d822cc94a295a2d904d1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-03-31 20:27:03 -0700

    React-Router: Organizing our Routes

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        This is my edit expense component
+    </div>
+);
+
+export default EditExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+    <div>
+        This is my help page component
+    </div>
+);
+
+export default HelpPage;
