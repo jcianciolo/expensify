4dbc33814d7b76c505e3e914204b4475fee56ef1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-03 09:32:01 -0700

    Redux: Dispatching Actions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import ExpenseDashboardPage from './components/ExpenseDashboardPage';
-import AddExpensePage from './components/AddExpensePage';
-import EditExpensePage from './components/EditExpensePage';
-import HelpPage from './components/HelpPage';
-import NotFoundPge from './components/NotFoundPage';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
