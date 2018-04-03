de2afe6e2a4523c268542af77df025a6e5c746b1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-03 14:01:56 -0700

    Redux: Sorting Redux Data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -137,6 +137,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -154,17 +160,17 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense( { description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense( { description: 'Rent', amount: 100, createdAt: -210000 }));
 const expenseTwo = store.dispatch(addExpense( { description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('ffe'));
+// store.dispatch(setTextFilter('ffe'));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount()); // amount
+store.dispatch(sortByAmount()); // amount
 // store.dispatch(sortByDate());   // date
 // store.dispatch(setStartDate(0));
 // // store.dispatch(setStartDate());
