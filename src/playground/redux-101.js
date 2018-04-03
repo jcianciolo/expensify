9ffa92fd026c0f8fcbec117a22357591fda7a181
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-03 10:42:22 -0700

    Redux: Refactoring and Organizing

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,25 +1,42 @@
 import { createStore } from 'redux';
 
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
 const store = createStore((state = { count : 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'SET':
             return {
                 count: action.count
             };
         case 'RESET':
-                return {
-                    count: 0
-                }
+            return {
+                count: 0
+            };
         default:
             return state;
     }
@@ -30,31 +47,20 @@ const unsubscribe = store.subscribe(() => {
 });
 
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
+
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+store.dispatch(incrementCount());
 
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-    type: 'RESET'
+store.dispatch(decrementCount());
 
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-    type: 'DECREMENT',
-});
-
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
-
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount({ count: 20 }));
