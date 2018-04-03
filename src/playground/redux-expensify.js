7ac0c8b9cb2f180751d949830c6fd127b92fc4c1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-03 13:55:27 -0700

    Redux: Filtering Redux Data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -129,6 +129,17 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 };
 
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    });
+};
+
 // Store creation
 const store = createStore(
     combineReducers({
@@ -138,24 +149,26 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense( { description: 'Rent', amount: 100 }));
-// const expenseTwo = store.dispatch(addExpense( { description: 'Coffee', amount: 300 }));
+const expenseOne = store.dispatch(addExpense( { description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseTwo = store.dispatch(addExpense( { description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-// store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter('ffe'));
 // store.dispatch(setTextFilter());
 
 // store.dispatch(sortByAmount()); // amount
 // store.dispatch(sortByDate());   // date
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setStartDate(0));
+// // store.dispatch(setStartDate());
+// store.dispatch(setEndDate(999));
 
 
 
@@ -175,13 +188,13 @@ const demoState = {
     }
 };
 
-const user = {
-    name: 'Jen',
-    age: 24
-};
+// const user = {
+//     name: 'Jen',
+//     age: 24
+// };
 
-console.log({
-   ...user,
-   location: 'Redwood City',
-   age: 25 
-});
+// console.log({
+//    ...user,
+//    location: 'Redwood City',
+//    age: 25 
+// });
