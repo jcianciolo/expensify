af0014034950e4869f6cb623da1677773428041c
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-03 11:27:07 -0700

    Redux: Working with Multiple Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,41 @@
 import { createStore, combineReducers } from 'redux';
 
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filters reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Store creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'awkaodkawodad',
