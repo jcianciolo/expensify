21db7ed52ef4887ce8fe72ae551a41c2a39158b1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-08 17:06:16 -0700

    React with Redux: Wiring up Add Expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -12,7 +12,8 @@ export default class ExpenseForm extends React.Component {
         note: '',
         amount: '',
         createdAt: moment(),
-        calendarFocused: false
+        calendarFocused: false,
+        error: ''
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -25,20 +26,38 @@ export default class ExpenseForm extends React.Component {
     onAmountChange = (e) => {
         const amount = e.target.value;
 
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {    // regex101.com
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {    // regex101.com
             this.setState(() => ({ amount }));
         }
     };
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt })); // ES6 object shorthand
+        if (createdAt) {
+            this.setState(() => ({ createdAt })); // ES6 object shorthand
+        }
     };
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: 'Please provide description and amount.'}));
+        } else {
+            this.setState(() => ({ error: '' }));
+            this.props.onSubmit({
+               description: this.state.description,
+               amount: parseFloat(this.state.amount, 10) * 100,
+               createdAt: this.state.createdAt.valueOf(),
+               note: this.state.note
+            });
+        }
+    };
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
