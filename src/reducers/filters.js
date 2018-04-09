88acb551ee303282c1306e51c737a5fc88150d12
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-09 10:20:27 -0700

    React with Redux: Filtering by Dates

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,11 @@
+import moment from 'moment';
 
 // Filters reducer
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')
 };
 
 export default (state = filtersReducerDefaultState, action) => {
