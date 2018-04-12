b197399723548a60f9f2a54182895407d0f4b4d2
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-12 11:58:20 -0700

    Deploying Apps: Creating Separate CSS Files Setup production webpack build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
-    
-    console.log('env', env);
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
     return {
         entry: './src/app.js',
         output: {
@@ -17,14 +18,28 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-            ]
-        }]
+                use: CSSExtract.extract({ 
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
         },
-        devtool: 'cheap-module-eval-source-map',
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
