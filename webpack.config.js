d38139275dc365e8fcbd2b3917e0a7015e61ea88
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-12 11:34:14 -0700

    Deploying Apps: Production Webpack Created separate build for production. Changed webpack.config.js from exporting an object to exporting a function that returns the object. This allows us to access the environment and use it in the object configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    
+    console.log('env', env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
             ]
         }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
-
