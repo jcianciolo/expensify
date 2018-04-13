3d0b7f05dd6be931cd81cf374b7fcb40af22bc1a
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-13 11:19:44 -0700

    Fetching data in Firebase and basic CRUD operations

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,29 +12,69 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'John Cianciolo',
-    age: 25,
-    isSingle: true,
-    location: {
-        city: 'Redwood City',
-        country: 'United States'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((e) => {
-    console.log('This failed.', e);
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
 
-// database.ref().set('This is my data.');
-// database.ref('age').set(26);
-// database.ref('location/city').set('San Mateo');
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
 
-database.ref('attributes').set({
-    height: 75,
-    weight: 180
-}).then(() => {
-    console.log('Saved new attributes');
-}).catch((e) => {
-    console.log('That failed.', e);
-});
+// setTimeout(() => {
+//     database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500);
+
+
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e);
+// });
+
+// database.ref().set({
+//     name: 'John Cianciolo',
+//     age: 25,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Redwood City',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle',
+// });
+
+// const isSingleData = firebase.database().ref();
+// isSingleData.remove()
+//     .then(() => {
+//         console.log('Data removed.');
+//     }).catch((e) => {
+//         console.log('error removing data: ', e);
+// });
