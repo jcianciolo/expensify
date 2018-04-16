520cfb9c12ecd78940cddca17f2ffa5322319ba9
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-16 13:27:34 -0700

    Remove Expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,16 @@ export const removeExpense = ({ id }= {}) => ({
     id
 });
 
+// START_REMOVE_EXPENSE
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
@@ -61,3 +71,4 @@ export const startSetExpenses = () => {
         });
     };
 };
+
