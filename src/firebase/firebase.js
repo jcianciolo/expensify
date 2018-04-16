3d2759877b3d92b284035c0147996f0ab247723f
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-16 11:22:43 -0700

    Creating a separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
-import moment from 'moment';
+// import moment from 'moment';
 
 const config = {
-    apiKey: "AIzaSyBNNr6hDZsLxeRCHYRXsWJVZBAPI9ws8ZM",
-    authDomain: "expensify-ea570.firebaseapp.com",
-    databaseURL: "https://expensify-ea570.firebaseio.com",
-    projectId: "expensify-ea570",
-    storageBucket: "expensify-ea570.appspot.com",
-    messagingSenderId: "182875561845"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -13,7 +13,11 @@ console.log('before');
 promise.then((data) => {
     console.log('1', data);
 
-    return 'some data';
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is my other promise');
+        }, 3500);
+    });
 }).then((str) => {
     console.log('does this run?', str);
 }).catch((error) => {
