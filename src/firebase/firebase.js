1a0186169612ec8bef347c241e8c5337bd916ca9
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2018-04-16 14:49:12 -0700

    Created Login Page and Google Authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase';
-// import moment from 'moment';
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -12,8 +11,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
